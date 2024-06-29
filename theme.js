a4c9f70053dd0533e276955c342d68045e3e8d97
100799 <ana.gomezvillamar@gmail.com>
2024-06-29 13:30:36 -0500

    CAMBIOS

--- theme.js
+++ theme.js
@@ -1,15 +1,19 @@
-// Importar SweetAlert
-const Swal = require('sweetalert2');
 
-// Crear popup
-Swal.fire({
-  title: 'Título del popup',
-  text: 'Contenido del popup',
-  icon: 'info',
-  confirmButtonText: 'Cerrar'
-});
-
-
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
 
 
 function DSpaceSetupAutocomplete(a, b) {
